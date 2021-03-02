c2e3216abe68e1050fb642dd80270aceb9b359eb
Simon <simonam@bellsouth.net>
2021-03-01 20:29:00 -0600

    UI made
    Started UI and made functioning button

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.tictactoe.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var binding: ActivityMainBinding
 
@@ -13,9 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        init()
     }
 
+    private fun init() {
+        binding.btn1.setOnClickListener(this)
+    }
 
- // asdfasasfa
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_1 -> {
+                binding.btn1.setBackgroundResource(R.drawable.ic_o)
+            }
+
+        }
+    }
+
 
 }
